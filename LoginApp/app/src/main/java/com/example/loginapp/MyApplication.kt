e968919136ca75afdcd42f2b9a3093fb775cb4c0
Roshan Nayak <roshannayak610@gmail.com>
2020-09-29 12:17:51 +0530

    close Realm Instance inside Application class.

--- LoginApp/app/src/main/java/com/example/loginapp/MyApplication.kt
+++ LoginApp/app/src/main/java/com/example/loginapp/MyApplication.kt
@@ -1,6 +1,8 @@
 package com.example.loginapp
 
 import android.app.Application
+import android.content.res.Configuration
+import com.example.loginapp.repository.UserDetailsRepository
 import io.realm.Realm
 
 class MyApplication : Application() {
@@ -9,5 +11,10 @@ class MyApplication : Application() {
         super.onCreate()
 
         Realm.init(this)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        UserDetailsRepository().closeRealm()
     }
 }
--- LoginApp/app/src/main/java/com/example/loginapp/ui/MainActivity.kt
+++ LoginApp/app/src/main/java/com/example/loginapp/ui/MainActivity.kt
@@ -3,18 +3,10 @@ package com.example.loginapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.loginapp.R
-import com.example.loginapp.repository.UserDetailsRepository
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        val userDetailsRepository =
-            UserDetailsRepository()
-        userDetailsRepository.closeRealm()
     }
 }
